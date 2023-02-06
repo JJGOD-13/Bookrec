dba3df2985606a8cb72960416f1d55d9c0e096ca
JJGOD <jayantgodse13@gmail.com>
2023-02-06 19:49:32 +1100

    small changes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import flask
 from cs50 import SQL
-from helpers import login_required
 
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -10,23 +9,11 @@ from flask_session import Session
 # configure application
 app = flask.Flask(__name__)
 
-# configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PerMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 # configure CS50 Library to use SQLite database
-db = SQL("sqlite:///people.db")
-
-
-@app.after_request
-def after_request(response):
-    """Disable caching"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+db = SQL("sqlite:///database.db")
 
 
 @app.route("/")
-@login_required
+def hello_world():
+    return "<p>Hello, world!<p>"
